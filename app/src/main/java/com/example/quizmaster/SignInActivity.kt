08ae5068fb15e909ffabf29b11ef3ad0f9070541
Subbulakashmi <subbulakshmi.arumugam20@gmail.com>
2022-06-24 03:22:20 +0530

    reset code added

--- app/src/main/java/com/example/quizmaster/SignInActivity.kt
+++ app/src/main/java/com/example/quizmaster/SignInActivity.kt
@@ -1,8 +1,14 @@
 package com.example.quizmaster
 
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizmaster.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,6 +17,7 @@ class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var reset_pwd: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +25,39 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
 
+        reset_pwd = findViewById(R.id.reset_pwd)
+        reset_pwd.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val email = binding.emailEt.text.toString()
+
+                if (email.toString().isNotEmpty()) {
+
+                    firebaseAuth.sendPasswordResetEmail(email.toString())
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val builder = AlertDialog.Builder(this@SignInActivity)
+                                builder.setMessage("Email Sent to ${email.toString()}")
+                                builder.setCancelable(true)
+                                builder.setNegativeButton("OK", DialogInterface.OnClickListener
+                                { dialog, which -> dialog.cancel() })
+                                val alertDialog: AlertDialog = builder.create()
+                                alertDialog.show()
+
+                                Log.d(ContentValues.TAG, "Email sent.")
+                            }
+                            startActivity(Intent(this@SignInActivity, SignInActivity::class.java))
+
+                        }
+                }
+            }
+        }
+
         binding.textView.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+//            val intent1 = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
+            startActivity(Intent(this@SignInActivity, SignUpActivity::class.java))
+
+
         }
 
         binding.button.setOnClickListener {
@@ -32,21 +69,23 @@ class SignInActivity : AppCompatActivity() {
                 firebaseAuth.signInWithEmailAndPassword(email, pass)
                     .addOnCompleteListener {
                         if (it.isSuccessful) {
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
+//                            val intent = Intent(this, MainActivity::class.java)
+//                            startActivity(intent)
+                            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+
                         } else {
 //                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
                     .addOnFailureListener {
                         Toast.makeText(
-                            this,
+                            this@SignInActivity,
                             "Failed to create user:${it.message}",
                             Toast.LENGTH_LONG
                         ).show()
                     }
             } else {
-                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SignInActivity, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
         }
@@ -60,4 +99,5 @@ class SignInActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+}
 }
