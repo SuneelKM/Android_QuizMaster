a7c4de63dac68b99cdaf4fb17855a636eec36170
SuneelM <mauryasuneel@outlook.com>
2022-06-24 02:07:04 -0400

    Add firebase realtime database

--- app/src/main/java/com/example/quizmaster/UserData/UserSignup.kt
+++ app/src/main/java/com/example/quizmaster/UserData/UserSignup.kt
@@ -3,8 +3,4 @@ package com.example.quizmaster.UserData
 data class UserSignup(
     val username: String,
     val email: String
-//    val date: String,
-//    val category: String,
-//    val level: String,
-//    val points: String
 )
